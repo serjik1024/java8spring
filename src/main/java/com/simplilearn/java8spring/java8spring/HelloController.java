c27528225d697bacde331d59370f4780bb809768
Serj <c-serjik.isagholian@hulu.com>
2020-11-25 23:49:48 -0800

    added hello controller

---
+++ src/main/java/com/simplilearn/java8spring/java8spring/HelloController.java
@@ -0,0 +1,14 @@
+package com.simplilearn.java8spring.java8spring;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
+	}
+
+}
