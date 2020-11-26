1d0a0df1c05ac8b1748224e2709193581ab89486
Serj <c-serjik.isagholian@hulu.com>
2020-11-25 20:59:31 -0800

    init

---
+++ src/main/java/com/simplilearn/java8spring/java8spring/Java8springApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.java8spring.java8spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java8springApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java8springApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/java8spring/java8spring/Java8springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.java8spring.java8spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java8springApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
